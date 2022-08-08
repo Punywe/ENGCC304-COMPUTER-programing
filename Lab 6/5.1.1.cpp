356915c87bbec7272beff9faa07e75b4018582f1
Punywe <108287015+Punywe@users.noreply.github.com>
2022-08-09 02:38:38 +0700

    Add files via upload

---
+++ Lab 6/5.1.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    int i;
+    int j;
+    int num;
+    char star = '*';
+    printf("Input your line : ");
+    scanf_s("%d", &num);
+    for (i = 1; i <= num; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%c", star);
+        } //end for
+        printf("\n");
+    } //end for
+    return 0;
+} //end funtion
+
